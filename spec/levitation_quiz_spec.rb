6bcfd3c4a3ed92208a5b46f4352be81c3235841f
Daniel Bovington <d.bovington@gmail.com>
2019-03-17 14:47:26 +0000

    Automatically backed up by Learn

--- spec/levitation_quiz_spec.rb
+++ spec/levitation_quiz_spec.rb
@@ -1,6 +1,17 @@
 require_relative "spec_helper"
 require_relative "../levitation_quiz.rb"
 
+def levitation_quiz
+  def annoying
+    loop do 
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
+end
+end
+
 describe "#levitation_quiz" do 
 	it "puts out the question 'What is the spell that enacts levitation?' until a user types 'Wingardium Leviosa'. Then, it puts out 'You passed the quiz!'" do 
 		allow(self).to receive(:gets).and_return("Wingardium Leviosa")
